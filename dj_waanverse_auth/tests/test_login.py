d3a5c318a09f45b18e7142ca8d0241a17348fc89
Khaotungkulmethee Pattawee <140291119+Theetawee@users.noreply.github.com>
2024-08-05 00:08:00 +0300

    save

--- dj_waanverse_auth/tests/test_login.py
+++ dj_waanverse_auth/tests/test_login.py
@@ -12,6 +12,7 @@ class LoginViewTests(TestSetup):
                 "password": "testpassword123",
             },
         )
+        print(response.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIn("access_token", response.data)
         self.assertIn("refresh_token", response.data)
@@ -31,49 +32,3 @@ class LoginViewTests(TestSetup):
         # Assert that the cookie values match the response data
         self.assertEqual(access_cookie.value, response.data["access_token"])
         self.assertEqual(refresh_cookie.value, response.data["refresh_token"])
-
-    def test_login_with_email(self):
-        response = self.client.post(
-            self.url,
-            {
-                "username": "test@example.com",
-                "password": "testpassword123",
-            },
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertIn("access_token", response.data)
-        self.assertIn("refresh_token", response.data)
-
-    def test_login_with_phone(self):
-        response = self.client.post(
-            self.url,
-            {
-                "username": "1234567890",
-                "password": "testpassword123",
-            },
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertIn("access_token", response.data)
-        self.assertIn("refresh_token", response.data)
-
-    def test_login_with_invalid_username(self):
-        response = self.client.post(
-            self.url,
-            {
-                "username": "invaliduser",
-                "password": "testpassword123",
-            },
-        )
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertIn("non_field_errors", response.data)
-
-    def test_login_with_invalid_password(self):
-        response = self.client.post(
-            self.url,
-            {
-                "username": "testuser",
-                "password": "wrongpassword",
-            },
-        )
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertIn("non_field_errors", response.data)
--- dj_waanverse_auth/tests/test_setup.py
+++ dj_waanverse_auth/tests/test_setup.py
@@ -1,8 +1,11 @@
 from django.test import TestCase
 from rest_framework.test import APIClient
 from django.urls import reverse
-from dj_waanverse_auth.models import Account
+from accounts.models import Account
 from dj_waanverse_auth.settings import accounts_config
+
+
+accounts_config["AUTHENTICATION_METHODS"] = ["username"]
 
 
 class TestSetup(TestCase):
@@ -16,7 +19,7 @@ class TestSetup(TestCase):
             password="testpassword123",
             date_of_birth="1990-01-01",
             pronouns="H",
-            phone="1234567890",
+            phone_number="1234567890",
         )
         self.access_cookie_name = accounts_config["ACCESS_TOKEN_COOKIE_NAME"]
         self.refresh_cookie_name = accounts_config["REFRESH_TOKEN_COOKIE_NAME"]
