281053f450da23357c917d7cb9a839c5137e1a44
Khaotungkulmethee Pattawee <140291119+Theetawee@users.noreply.github.com>
2024-08-11 10:33:40 +0300

    new tests

---
+++ tests/login/test_login_view.py
@@ -0,0 +1,27 @@
+from dj_waanverse_auth.settings import accounts_config
+
+from .test_setup import TestSetup
+
+
+class TestLoginView(TestSetup):
+    def test_login_view_non_verified(self):
+        user_data = {
+            "login_field": self.user1.username,
+            "password": "password1",
+        }
+        response = self.client.post(self.login_url, user_data, format="json")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("email", response.data)
+        self.assertIn("status", response.data)
+        self.assertEqual(response.data["status"], "unverified")
+        self.assertEqual(response.data["email"], self.user1.email)
+
+    def test_login_view_verified_email(self):
+        accounts_config.AUTH_METHODS = ["email"]
+        user_data = {
+            "login_field": self.user2.email,
+            "password": "password2",
+        }
+        response = self.client.post(self.login_url, user_data, format="json")
+        self.assert_auth_cookies(response)
+        self.assert_auth_response(response)
---
+++ tests/login/test_setup.py
@@ -0,0 +1,39 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APIClient
+
+from dj_waanverse_auth.models import EmailAddress
+from dj_waanverse_auth.settings import accounts_config
+
+Account = get_user_model()
+
+
+class TestSetup(TestCase):
+    def setUp(self):
+        self.login_url = reverse("login")
+        self.client = APIClient()
+        self.access_cookie_name = accounts_config.ACCESS_TOKEN_COOKIE
+        self.refresh_cookie_name = accounts_config.REFRESH_TOKEN_COOKIE
+        self.user1 = Account.objects.create_user(
+            username="user1", password="password1", email="a@a.com", name="user1"
+        )
+
+        self.user2 = Account.objects.create_user(
+            username="user2", password="password2", email="b@b.com", name="user2"
+        )
+
+        EmailAddress.objects.create(
+            user=self.user2, email="b@b.com", verified=True, primary=True
+        ).save()
+
+    def assert_auth_cookies(self, response):
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(self.access_cookie_name, response.cookies)
+        self.assertIn(self.refresh_cookie_name, response.cookies)
+
+    def assert_auth_response(self, response):
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("access_token", response.data)
+        self.assertIn("refresh_token", response.data)
+        self.assertIn("user", response.data)
