a4200b3539b779b2e530ba96d69ea6998bbbe948
Khaotungkulmethee Pattawee <140291119+Theetawee@users.noreply.github.com>
2024-08-15 07:22:54 +0300

    Update messages.py

--- dj_waanverse_auth/messages.py
+++ dj_waanverse_auth/messages.py
@@ -1,4 +1,5 @@
 from typing import TypedDict
+from django.utils.translation import ugettext_lazy as _
 
 
 class MessagesSchema(TypedDict):
@@ -36,42 +37,52 @@ class MessagesSchema(TypedDict):
 
 
 class Messages(MessagesSchema):
-    status_unverified = (
-        "Your email is unverified. Please verify your email to continue."
+    status_unverified = _("Your email remains unverified. Please verify it to proceed.")
+    status_verified = _("Your email has been successfully verified.")
+    no_account = _("No active account found.")
+    email_sent = _("A verification email has been dispatched to your email address.")
+    email_required = _("A valid email address is required to continue.")
+    mfa_enabled_success = _(
+        "Multi-factor authentication has been successfully enabled."
     )
-    status_verified = "Your email has been successfully verified."
-    no_account = "No active account is found."
-    email_sent = "A verification email has been sent to your email address."
-    email_required = "A valid email address is required."
-    mfa_enabled_success = "Multi-factor authentication has been successfully enabled."
-    mfa_already_activated = "Multi-factor authentication is already activated."
-    mfa_invalid_otp = "The OTP provided is invalid. Please try again."
-    mfa_not_activated = "Multi-factor authentication is not activated for this account."
-    mfa_deactivated = "Multi-factor authentication has been deactivated."
-    mfa_login_failed = "MFA login failed. Invalid OTP or recovery code."
-    mfa_login_success = "MFA login successful."
-    invalid_account = "The account provided is invalid. Please try again."
-    password_reset_code_sent = (
+    mfa_already_activated = _("Multi-factor authentication is already activated.")
+    mfa_invalid_otp = _("The provided OTP is invalid. Please try again.")
+    mfa_not_activated = _(
+        "Multi-factor authentication is not activated for this account."
+    )
+    mfa_deactivated = _(
+        "Multi-factor authentication has been successfully deactivated."
+    )
+    mfa_login_failed = _("MFA login failed due to an invalid OTP or recovery code.")
+    mfa_login_success = _("MFA login was successful.")
+    invalid_account = _("The provided account details are invalid. Please try again.")
+    password_reset_code_sent = _(
         "A password reset code has been sent to your email address."
     )
-    password_reset_successful = "Your password has been successfully reset."
-    already_authenticated = "You are already authenticated."
-    logout_successful = "You have been successfully logged out."
-    mfa_required = "Multi-factor authentication is required for this action."
-    mfa_recovery_codes_generated = (
+    password_reset_successful = _("Your password has been reset successfully.")
+    already_authenticated = _("You are already authenticated.")
+    logout_successful = _("You have successfully logged out.")
+    mfa_required = _("Multi-factor authentication is required for this action.")
+    mfa_recovery_codes_generated = _(
         "New MFA recovery codes have been successfully generated."
     )
-    mfa_setup_failed = "An error occurred while setting up multi-factor authentication."
-    recovery_codes_regeneration_error = (
+    mfa_setup_failed = _(
+        "An error occurred during the setup of multi-factor authentication."
+    )
+    recovery_codes_regeneration_error = _(
         "An error occurred while generating recovery codes. Please try again."
     )
-    token_error = "An error occurred while processing the token. Please try again."
-    general_msg = "An error occurred. Please try again."
-    mfa_code_generated_email_subject = "New MFA Recovery Codes Generated"
-    email_already_verified = "Your email is already verified."
-    no_credentials = "No valid credentials provided. Please try again."
-    user_creation_error = "An error occurred while creating the user. Please try again."
-    attempts_limit = "Too many attempts. Please try again after the cooldown period."
-    invalid_password = "Invalid password."
-    mfa_deactivated_email_subject = "Multi-factor authentication deactivated"
-    reset_password_email_subject = "Password Reset Request"
+    token_error = _("An error occurred while processing the token. Please try again.")
+    general_msg = _("An error occurred. Please try again later.")
+    mfa_code_generated_email_subject = _(
+        "New Multi-factor Authentication Recovery Codes Generated"
+    )
+    email_already_verified = _("Your email is already verified.")
+    no_credentials = _("No valid credentials provided. Please check and try again.")
+    user_creation_error = _("An error occurred during user creation. Please try again.")
+    attempts_limit = _("Too many attempts. Please try again after the cooldown period.")
+    invalid_password = _("The password provided is invalid.")
+    mfa_deactivated_email_subject = _(
+        "Multi-factor Authentication Deactivation Confirmation"
+    )
+    reset_password_email_subject = _("Password Reset Request")
