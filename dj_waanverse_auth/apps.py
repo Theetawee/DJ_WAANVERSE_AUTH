c8b7d0fef79b916d8c020562a93f54b36f1b7d2d
Pattawee Drake Khaotungkulmethee <140291119+Theetawee@users.noreply.github.com>
2025-01-03 22:34:52 +0300

    save

--- dj_waanverse_auth/apps.py
+++ dj_waanverse_auth/apps.py
@@ -8,9 +8,3 @@ class AccountsConfig(AppConfig):
     name = "dj_waanverse_auth"
     label = "dj_waanverse_auth"
     verbose_name = "Waanverse Auth"
-
-    # def ready(self):
-    #     from .signals import (
-    #         log_user_logged_in_success,
-    #         user_created_via_google,
-    #     )
