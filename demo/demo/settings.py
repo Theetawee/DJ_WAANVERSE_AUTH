aa3b4a3d4623179f88b78154557c01a1915e12b8
Pattawee Drake Khaotungkulmethee <140291119+Theetawee@users.noreply.github.com>
2025-01-05 05:31:04 +0300

    save

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -54,7 +54,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "dj_waanverse_auth.middleware.WaanverseAuthMiddleware",
 ]
 
 ROOT_URLCONF = "demo.urls"
--- dj_waanverse_auth/views/authorization_views.py
+++ dj_waanverse_auth/views/authorization_views.py
@@ -9,25 +9,25 @@ from dj_waanverse_auth.settings import auth_config
 
 
 @api_view(["POST"])
-@permission_classes([IsAuthenticated])
+@permission_classes([AllowAny])
 def refresh_access_token(request):
-    refresh_token = request.COOKIES.get("refresh_token", None) or request.data.get(
-        "refresh_token", None
-    )
-
-    if not refresh_token:
-        return Response(
-            {"error": "Refresh token is required."},
-            status=status.HTTP_400_BAD_REQUEST,
-        )
+    refresh_token = request.COOKIES.get(
+        auth_config.refresh_token_cookie, None
+    ) or request.data.get("refresh_token", None)
 
     token_service = TokenService(refresh_token=refresh_token)
 
+    if not refresh_token:
+        return token_service.delete_tokens_from_response(
+            response=Response(
+                {"error": "Refresh token is required."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        )
+
     try:
-        # Generate new tokens
         tokens = token_service.generate_tokens()
 
-        # Prepare the response and add the tokens as cookies
         response = Response(
             data={"access_token": tokens["access_token"]},
             status=status.HTTP_200_OK,
